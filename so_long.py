64dff06c6b07b8e227b159d745067dfed0fb851b
elchubinebrae <waitesdennis@gmail.com>
2024-01-26 23:39:18 +0000

    spelt he names of the libraries correctly and adde

--- so_long.py
+++ so_long.py
@@ -1,7 +1,10 @@
-import pandas a s pd
-import nunpy as np
+import pandas as pd
+import numpy as np
 
 #This is just a comment, hopefullt it wont go wrong
 
 df = pd.DataFrame({"Thing":[1,2,3,4,5], "Thing2":[4,5,6,7,8]})
 
+df2 = pd.DataFrame()
+
+df3 = ""
